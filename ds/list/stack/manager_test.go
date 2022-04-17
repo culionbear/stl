91cf956bbe18ce65917b9abd056226ed2a38ced5
彭芳洁 <1639579565@qq.com>
2022-04-17 16:12:36 +0800

    # stack 单元测试

--- ds/list/stack/manager_test.go
+++ ds/list/stack/manager_test.go
@@ -11,3 +11,61 @@ func TestIterator(t *testing.T) {
 		fmt.Println(it.Value())
 	}
 }
+
+func TestPush(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	s.Push(2)
+	for it := s.Begin(); it.End(); it = it.Next() {
+		fmt.Println(it.Value())
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := New[int]()
+	fmt.Println(s.Pop())
+	s.PushMore(1, 2, 3)
+	for s.Size() != 0 {
+		fmt.Println(s.Pop())
+	}
+}
+
+func TestPushMore(t *testing.T) {
+	s := New[int]()
+	s.PushMore(1, 2, 3, 4, 8, 99, -1)
+	for it := s.Begin(); it.End(); it = it.Next() {
+		fmt.Println(it.Value())
+	}
+}
+
+func TestSize(t *testing.T) {
+	s := New[int]()
+	fmt.Println(s.Size())
+	s.PushMore(1, 2, 3, 4)
+	fmt.Println(s.Size())
+}
+
+func TestFirst(t *testing.T) {
+	s := New[int]()
+	fmt.Println(s.First())
+	s.PushMore(1, 2, 3, 4)
+	fmt.Println(s.First())
+
+	sta := New[string]()
+	sta.PushMore("111", "fang", "bear")
+	fmt.Println(sta.First())
+}
+
+func TestTop(t *testing.T) {
+	sta := New[string]()
+	fmt.Println(sta.Top())
+	sta.PushMore("111", "fang", "bear")
+	fmt.Println(sta.Top())
+}
+
+func TestIsEmpty(t *testing.T) {
+	s := New[int]()
+	fmt.Println(s.IsEmpty())
+	s.Push(1)
+	fmt.Println(s.IsEmpty())
+}
